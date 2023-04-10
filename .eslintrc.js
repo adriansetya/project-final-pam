3a7ebf20e7e7d6b47a3b89802e3b95b83ea5ecf0
adriansetya <adriansetyarr@gmail.com>
2023-04-10 11:04:44 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return <Text>Hello World! 567</Text>;
+};
+
+export default App;
